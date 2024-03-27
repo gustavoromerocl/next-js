849af5534613be5e823dfc97863be2b0d889962c
romerodevelopments <gandresrp@gmail.com>
2024-03-27 08:06:59 -0300

    introduccion a nextjs - config layout

---
+++ app/(general)/layout.tsx
@@ -0,0 +1,13 @@
+
+export default function AboutLayout({
+ children
+}: {
+ children: React.ReactNode;
+}) {
+  return (
+    <div>
+      <h1>Hello Root Layout About</h1>
+      {children}
+    </div>
+  );
+}
