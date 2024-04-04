14d55daa1f2a5cc5f5ed97af1f2e2c2c27cde661
romerodevelopments <gandresrp@gmail.com>
2024-04-04 08:52:58 -0300

    Despliegues a Vercel y docker images

--- 01-first-steps/app/page.tsx
+++ 01-first-steps/app/page.tsx
@@ -1,9 +1,12 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <span className="text-5xl">Hola mundo</span>
+
+      <Link href={'/about'}>About page</Link>
     </main>
   );
 }
