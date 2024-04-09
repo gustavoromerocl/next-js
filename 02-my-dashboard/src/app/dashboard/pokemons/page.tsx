768deb38ce1b981f62440b47339727e4888adb8f
romerodevelopments <gandresrp@gmail.com>
2024-04-09 08:49:01 -0400

    generacion dinamica ssr: asignar tipo de datos y mostrar imagenes

--- 02-my-dashboard/src/app/dashboard/pokemons/page.tsx
+++ 02-my-dashboard/src/app/dashboard/pokemons/page.tsx
@@ -1,15 +1,31 @@
-const getPokemons = async (limit = 20, offset = 0) => {
-  const data = await fetch(`https://pokeapi.co/api/v2/pokemon?limit=${limit}&offset=${offset}`)
+import { PokemonsResponse, SimplePokemon } from "@/app/pokemons";
+import Image from "next/image";
+
+const getPokemons = async (limit = 151, offset = 0): Promise<SimplePokemon[]> => {
+  const data: PokemonsResponse = await fetch(`https://pokeapi.co/api/v2/pokemon?limit=${limit}&offset=${offset}`)
     .then(res => res.json());
 
-  return data;
+  return data.results.map(pokemon => ({ id: pokemon.url.split('/').at(-2) as string, name: pokemon.name }));
 }
 
 export default async function PokemonsPage() {
   const pokemons = await getPokemons();
   return (
-    <div>
-      {JSON.stringify(pokemons)}
+    <div className="flex flex-col">
+      <div className="flex flex-wrap gap-10 items-center justify-center">
+        {
+          pokemons.map(({id, name}) =>
+            <Image
+              key={id}
+              src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${id}.svg`}
+              width={100}
+              height={100}
+              alt={name}
+            />
+          )
+        }
+
+      </div>
     </div>
   );
 }
