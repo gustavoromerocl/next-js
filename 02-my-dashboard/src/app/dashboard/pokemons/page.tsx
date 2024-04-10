f78aac7ba7110b5d8b5e12f801771ca4acd22457
romerodevelopments <gandresrp@gmail.com>
2024-04-10 08:42:09 -0400

    generacion dinamica ssr: pensemos en componentes pequenos

--- 02-my-dashboard/src/app/dashboard/pokemons/page.tsx
+++ 02-my-dashboard/src/app/dashboard/pokemons/page.tsx
@@ -1,4 +1,4 @@
-import { PokemonsResponse, SimplePokemon } from "@/app/pokemons";
+import { PokemonsGrid, PokemonsResponse, SimplePokemon } from "@/app/pokemons";
 import Image from "next/image";
 
 const getPokemons = async (limit = 151, offset = 0): Promise<SimplePokemon[]> => {
@@ -12,20 +12,8 @@ export default async function PokemonsPage() {
   const pokemons = await getPokemons();
   return (
     <div className="flex flex-col">
-      <div className="flex flex-wrap gap-10 items-center justify-center">
-        {
-          pokemons.map(({id, name}) =>
-            <Image
-              key={id}
-              src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${id}.svg`}
-              width={100}
-              height={100}
-              alt={name}
-            />
-          )
-        }
-
-      </div>
+      <span className="text-5xl my-2">Listado de Pokémons <small>estático</small></span>
+      <PokemonsGrid pokemons={pokemons}/>
     </div>
   );
 }
--- 02-my-dashboard/src/app/pokemons/index.ts
+++ 02-my-dashboard/src/app/pokemons/index.ts
@@ -1,2 +1,3 @@
 export type { PokemonsResponse } from './interfaces/pokemon-response'
 export type { SimplePokemon } from './interfaces/simple-pokemon'
+export { PokemonsGrid } from './components/PokemonsGrid'
