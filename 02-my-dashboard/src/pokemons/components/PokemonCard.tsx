d83288d876e7a427718b7ad9745320845ef98b20
romerodevelopments <gandresrp@gmail.com>
2024-05-23 08:41:58 -0400

    generacion dinamica ssr: pantalla del pokemon

--- 02-my-dashboard/src/pokemons/components/PokemonCard.tsx
+++ 02-my-dashboard/src/pokemons/components/PokemonCard.tsx
@@ -25,15 +25,16 @@ export const PokemonCard = ({ pokemon }: Props) => {
           <p className="pt-2 text-lg font-semibold text-gray-50 capitalize">{name}</p>
           <p className="text-sm text-gray-100">John@Doe.com</p>
           <div className="mt-5">
-            <a
+          <Link
+              href={`dashboard/pokemon/${ id }`}
               className="border rounded-full py-2 px-4 text-xs font-semibold text-gray-100"
             >
               Más información
-            </a>
+            </Link>
           </div>
         </div>
         <div className="border-b">
-          <Link href={`/dashboard/pokemons/${id}`} className="px-4 py-2 hover:bg-gray-100 flex items-center">
+          <Link href={`/dashboard/main`} className="px-4 py-2 hover:bg-gray-100 flex items-center">
           <div className="text-red-600">
               <IoHeartOutline/>
             </div>
