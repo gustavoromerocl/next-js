ed9bce3cb176ad6e0b47dd0f06b7752bfa8c383b
romerodevelopments <gandresrp@gmail.com>
2024-05-23 08:27:57 -0400

    generacion dinamica ssr: metadata dinamica

--- 02-my-dashboard/src/app/dashboard/pokemon/[id]/page.tsx
+++ 02-my-dashboard/src/app/dashboard/pokemon/[id]/page.tsx
@@ -1,12 +1,24 @@
 import { Pokemon } from "@/pokemons";
+import { Metadata } from "next";
 
 interface Props {
   params: { id: string };
 }
 
+export async function generateMetadata({params}: Props): Promise<Metadata> {
+  const { id, name} = await getPokemon(params.id)
+  return {
+    title: `#${id} - ${name}`,
+    description: `Página del pokemon ${name}`
+  }
+}
+
 const getPokemon = async (id: string): Promise<any> => {
   const pokemon = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`, {
     cache: 'force-cache' //TODO: cambiar esto posteriormente
+    // next: {
+    //   revalidate: 60 * 60* 30 * 6 //revalidar cada 6 meses
+    // }
   }).then(resp => resp.json())
   console.log("POKEMON LOG", pokemon);
   return pokemon;
