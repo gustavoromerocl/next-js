4ed717e5ba9d3d278a9c13f1e26ab48dac20d04b
romerodevelopments <gandresrp@gmail.com>
2024-05-28 22:12:36 -0400

    generacion dinamica ssr: not found page - 404

--- 02-my-dashboard/src/app/dashboard/pokemon/[id]/page.tsx
+++ 02-my-dashboard/src/app/dashboard/pokemon/[id]/page.tsx
@@ -1,6 +1,7 @@
 import { Pokemon } from "@/pokemons";
 import { Metadata } from "next";
 import Image from "next/image";
+import { notFound } from "next/navigation";
 
 interface Props {
   params: { id: string };
@@ -15,14 +16,20 @@ export async function generateMetadata({ params }: Props): Promise<Metadata> {
 }
 
 const getPokemon = async (id: string): Promise<any> => {
-  const pokemon = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`, {
-    cache: 'force-cache' //TODO: cambiar esto posteriormente
-    // next: {
-    //   revalidate: 60 * 60* 30 * 6 //revalidar cada 6 meses
-    // }
-  }).then(resp => resp.json())
-  console.log("POKEMON LOG", pokemon);
-  return pokemon;
+  try {
+    const pokemon = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`, {
+      cache: 'force-cache' //TODO: cambiar esto posteriormente
+      // next: {
+      //   revalidate: 60 * 60* 30 * 6 //revalidar cada 6 meses
+      // }
+    }).then(resp => resp.json())
+    console.log("POKEMON LOG", pokemon);
+    return pokemon;
+  } catch(error) {
+    console.log(error)
+    notFound()
+  }
+
 }
 
 
